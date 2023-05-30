731e33bef277b0a9ae1a9cd3d178954dadd5e1fd
joaopedropc7 <joaopedropcunha7@gmail.com>
2023-05-29 22:12:23 -0300

    Initial commit

---
+++ src/main/java/med/voll/api/domain/paciente/DadosCadastroPaciente.java
@@ -0,0 +1,27 @@
+package med.voll.api.domain.paciente;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import med.voll.api.domain.endereco.DadosEndereco;
+
+public record DadosCadastroPaciente(
+
+        @NotBlank(message = "Nome é obrigatório")
+        String nome,
+
+        @NotBlank(message = "Email é obrigatório")
+        @Email(message = "Formato do email é inválido")
+        String email,
+
+        @NotBlank(message = "Telefone é obrigatório")
+        String telefone,
+
+        @NotBlank(message = "CPF é obrigatório")
+        String cpf,
+
+        @NotNull(message = "Dados do endereço são obrigatórios")
+        @Valid
+        DadosEndereco endereco) {
+}
