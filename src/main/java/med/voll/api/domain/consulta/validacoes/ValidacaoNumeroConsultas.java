c11ca8deaaf2d59615463080a1919f99e4ac9fc2
joaopedropc7 <joaopedropcunha7@gmail.com>
2023-05-30 16:09:14 -0300

    a

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoNumeroConsultas.java
@@ -0,0 +1,26 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import jakarta.validation.Valid;
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.ConsultaRepository;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidacaoNumeroConsultas implements ValidadorAgendamentoDeConsulta {
+
+
+    private ConsultaRepository repository;
+
+    public void validar(DadosAgendamentoConsulta dados){
+
+        var primeiroHorario = dados.data().withHour(7);
+        var ultimoHorario = dados.data().withHour(18);
+        var pacientePossuiOutraConsultaNoDia = repository.existsByPacienteIdAndDataBetween(dados.idPaciente(), primeiroHorario, ultimoHorario);
+
+        if (pacientePossuiOutraConsultaNoDia){
+            throw new ValidacaoException("Paciente ja possui uma consulta agendada nesse dia");
+        }
+    }
+
+}
